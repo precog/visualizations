884d4ce337d6f4c15aadb572cd39e69dca239e57
Franco Ponticelli <franco.ponticelli@gmail.com>
2011-12-02 14:04:17 -0700

    debugging template

--- staticrenderer/template/xml.template.php
+++ staticrenderer/template/xml.template.php
@@ -10,7 +10,8 @@ for($i=0;$i<count($css = $config->css());$i++)
 ?>
 </head>
 <body<?=$config->backgroundColor() ? ' bgcolor="'.$config->backgroundColor().'"' : ''?>>
-$config->backgroundColor();
+<?=$config->backgroundColor() ? ' bgcolor="'.$config->backgroundColor().'"' : ''?>
+<?php var_dump($config) ?>
 <<?=$config->element()?><?=$config->id()?' id="'.$config->id().'"':''?> class="rg<?=$config->className()?' '.$config->className():''?>"><?=$config->xml()?></<?=$config->element()?>>
 <script type="text/javascript"><![CDATA[
 setTimeout(function() { RG_READY = true; }, 200);
