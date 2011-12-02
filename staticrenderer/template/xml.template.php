161b547a8d9cd95a94a565f52d3001629f99b751
Franco Ponticelli <franco.ponticelli@gmail.com>
2011-12-02 14:34:40 -0700

    six for background (removed debug info)

--- staticrenderer/template/xml.template.php
+++ staticrenderer/template/xml.template.php
@@ -10,7 +10,6 @@ for($i=0;$i<count($css = $config->css());$i++)
 ?>
 </head>
 <body<?=($config->backgroundColor() ? (' bgcolor="'.$config->backgroundColor().'" style="background-color:' . $config->backgroundColor() .'"') : '')?>>
-<?=$config->format()?>
 <<?=$config->element()?><?=$config->id()?' id="'.$config->id().'"':''?> class="rg<?=$config->className()?' '.$config->className():''?>"<?=($config->backgroundColor() ? (' style="background-color:' . $config->backgroundColor() .'"') : '')?>><?=$config->xml()?></<?=$config->element()?>>
 <script type="text/javascript"><![CDATA[
 setTimeout(function() { RG_READY = true; }, 200);
