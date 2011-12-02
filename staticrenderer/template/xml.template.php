9b5b7a461ce472c4851245d72a676371f5575346
Franco Ponticelli <franco.ponticelli@gmail.com>
2011-12-02 14:12:11 -0700

    still on bg

--- staticrenderer/template/xml.template.php
+++ staticrenderer/template/xml.template.php
@@ -9,10 +9,10 @@ for($i=0;$i<count($css = $config->css());$i++)
 }
 ?>
 </head>
-<body<?=$config->backgroundColor() ? ' bgcolor="'.$config->backgroundColor().'"' : ''?>>
-<?=$config->backgroundColor() ? ' bgcolor="'.$config->backgroundColor().'"' : ''?><br/>
+<body<?=($config->backgroundColor() ? (' bgcolor="'.$config->backgroundColor().'"') : '')?>>
+<?=($config->backgroundColor() ? (' bgcolor="'.$config->backgroundColor().'"') : '')?><br/>
 COLOR: <?=$config->backgroundColor()?><br/>
-<pre><?php var_dump($config) ?></pre>
+<pre><?php var_dump($config); ?></pre>
 <<?=$config->element()?><?=$config->id()?' id="'.$config->id().'"':''?> class="rg<?=$config->className()?' '.$config->className():''?>"><?=$config->xml()?></<?=$config->element()?>>
 <script type="text/javascript"><![CDATA[
 setTimeout(function() { RG_READY = true; }, 200);
