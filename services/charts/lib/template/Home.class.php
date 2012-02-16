ec0e91ce26495e2563704f1cede51de4368d9efc
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-02-16 14:57:29 -0700

    more fixes to paths

--- services/charts/lib/template/Home.class.php
+++ services/charts/lib/template/Home.class.php
@@ -96,6 +96,17 @@ class template_Home extends erazor_macro_Template {
 				$__b__->b .= $x;
 			}
 			{
+				$x = $__context__->baseurl;
+				if(is_null($x)) {
+					$x = "null";
+				} else {
+					if(is_bool($x)) {
+						$x = (($x) ? "true" : "false");
+					}
+				}
+				$__b__->b .= $x;
+			}
+			{
 				$x = $__context__->url->route(_hx_anonymous(array("controller" => "uploadForm", "action" => "display")));
 				if(is_null($x)) {
 					$x = "null";
@@ -141,6 +152,17 @@ class template_Home extends erazor_macro_Template {
 			}
 			{
 				$x = "</a></li>\x0A  <li>Upload using a form and display result: <a href=\"";
+				if(is_null($x)) {
+					$x = "null";
+				} else {
+					if(is_bool($x)) {
+						$x = (($x) ? "true" : "false");
+					}
+				}
+				$__b__->b .= $x;
+			}
+			{
+				$x = $__context__->baseurl;
 				if(is_null($x)) {
 					$x = "null";
 				} else {
@@ -328,6 +350,17 @@ class template_Home extends erazor_macro_Template {
 			}
 			{
 				$x = "</dd>\x0A    </dl>\x0A  </li>\x0A</ul>\x0A<h3>GIST</h3>\x0A<ul class=\"bullet\">\x0A  <li>Upload GIST using a form: <a href=\"";
+				if(is_null($x)) {
+					$x = "null";
+				} else {
+					if(is_bool($x)) {
+						$x = (($x) ? "true" : "false");
+					}
+				}
+				$__b__->b .= $x;
+			}
+			{
+				$x = $__context__->baseurl;
 				if(is_null($x)) {
 					$x = "null";
 				} else {
