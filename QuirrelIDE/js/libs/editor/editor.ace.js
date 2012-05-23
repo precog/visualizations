58a23ad26ec59954eb60fbb64eb85250b1e8175f
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-05-23 16:02:40 -0600

    aired ace editor

---
+++ QuirrelIDE/js/libs/editor/editor.ace.js
@@ -0,0 +1,65 @@
+define([
+      "order!editor/ace/ace-uncompressed-noconflict"
+    , "order!editor/ace/mode-quirrel-uncompressed"
+],
+
+function(a) {
+    console.log(a);
+    return function(el) {
+        var wrapper,
+            editor = ace.edit($(el).get(0));
+        editor.commands.addCommand({
+            bindKey: {
+                win: 'Shift-Return',
+                mac: 'Shift-Return|Command-Return',
+                sender: 'editor|cli'
+            },
+            exec: function() {
+                $(wrapper).trigger('execute', wrapper.get());
+            }
+        });
+        editor.setShowPrintMargin(false);
+        var sess = editor.getSession();
+        sess.setMode(new (require("ace/mode/quirrel").Mode)());
+        sess.getSelection().on("changeCursor", function() {
+            $(wrapper).trigger("changeCursor", editor.getCursorPosition());
+        });
+        sess.getSelection().on("changeSelection", function() {
+            // TODO GET SELECTION NOT POSITION
+            $(wrapper).trigger("changeSelection", editor.getCursorPosition());
+        });
+        sess.on("change", (function() {
+            var kill;
+
+            function trigger() {
+                $(wrapper).trigger("changed", wrapper.get());
+            };
+
+            return function() {
+                clearInterval(kill);
+                kill = setTimeout(trigger, 250);
+            };
+        })());
+
+        wrapper = {
+            get : function() {
+                return sess.getValue(); //editor.getSession()
+            },
+            set : function(code) {
+                sess.setValue(code);
+            },
+            setTabSize : function(size) {
+                if(size === sess.getTabSize()) return;
+                sess.setTabSize(size);
+                $(wrapper).trigger("tabSizeChanged", size);
+            },
+            setUseSoftTabs : function(toogle) {
+                if(toogle === sess.getUseSoftTabs()) return;
+                sess.setUseSoftTabs(toogle);
+                $(wrapper).trigger("useSoftTabsChanged", toogle);
+            }
+        };
+
+        return wrapper;
+    }
+});
